57018d0f15518618a5b7b4c5f67ac1c238bffe0e
Gianluca Esposito <g@esposi.to>
2018-04-09 18:16:53 +0200

    Add Rollup with a sample React component

---
+++ scripts/rollup.config.js
@@ -0,0 +1,27 @@
+import nodeResolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import babel from "rollup-plugin-babel";
+
+export default {
+  input: "./src/index.js",
+  output: {
+    file: "dist/react-demo.cjs.min.js",
+    format: "cjs",
+    name: "ReactDemo",
+    globals: {
+      "react": "React",
+      "react-dom": "ReactDOM"
+    }
+  },
+  external: ["react", "react-dom"],
+  plugins: [
+    nodeResolve(),
+    commonjs({
+      include: "node_modules/**"
+    }),
+    babel({
+      exclude: "node_modules/**"
+    })
+  ],
+  sourceMap: true
+};
---
+++ src/Demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+    render() {
+        return (
+            <h1>
+               It works! 
+            </h1>
+        );
+    }
+}
+
+export default Demo;
